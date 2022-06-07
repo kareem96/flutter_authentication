e672e8033c0cf6425b83dfb9655f8731eb658363
kareem96 <ak339444@gmail.com>
2022-06-07 16:47:07 +0700

    update 07/06/2022

---
+++ test/helpers/test_mock.dart
@@ -0,0 +1,9 @@
+import 'package:flutter_auth/data/data.dart';
+import 'package:flutter_auth/domain/domain.dart';
+import 'package:mockito/annotations.dart';
+
+@GenerateMocks([
+  AuthRepository,
+  AuthRemoteDatasource,
+])
+void main(){}
